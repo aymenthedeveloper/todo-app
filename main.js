420197d6992e065bcb649a2524483ca2bd103c60
aymenthedeveloper <aymendev.contact@gmail.com>
2024-11-09 09:08:53 +0000

    added drag and drop functionality

--- main.js
+++ main.js
@@ -2,29 +2,156 @@ const toggleBtn = document.querySelector('.todo-app .app-header .toggle');
 const todoInput = document.querySelector('.todo-app .app-body .todo-input input')
 const todoAddBtn = document.querySelector('.todo-app .app-body .todo-input .todo-add-btn')
 const todoList = document.querySelector('.todo-app .app-body .todo-list')
+const todoFooter = document.querySelector('.todo-app .app-body .todo-footer')
+const itemsLeftElement = document.querySelector('.todo-app .app-body .todo-footer .todo-count .count')
+let itemsLeftNumber = 0;
+let startTime, currentTime;
+let currentDragElement;
 
-console.log(todoAddBtn);
 
+const todos = [
+  {
+    completed: true,
+    text: 'Complete online JavaScript course'
+  },
+  {
+    completed: false,
+    text: 'Jog around the park 3x'
+  },
+  {
+    completed: false,
+    text: '10 minutes meditation'
+  },
+  {
+    completed: false,
+    text: 'Read for 1 hour'
+  },
+  {
+    completed: false,
+    text: 'Pick up groceries'
+  },
+  {
+    completed: false,
+    text: 'Complete Todo App on Frontend Mentor'
+  },
+]
 
 toggleBtn.addEventListener('click', toggleTheme)
 todoInput.addEventListener('keydown', addTask)
 todoAddBtn.addEventListener('click', addTask)
+todoList.addEventListener('click', handleTodolistClicks)
+todoList.addEventListener('dragover', handleDragOver)
+todoFooter.addEventListener('click', handleFooterClicks)
+
+function handleDragOver(e){
+  e.preventDefault();
+  currentTime = new Date().getTime();
+  const time = currentTime - startTime;
+  if (time > 150){
+    startTime = new Date().getTime();
+    const afterElement = getDragAfterElement(e.clientY);
+    if (afterElement == null) {
+        todoList.appendChild(currentDragElement);
+    } else {
+        todoList.insertBefore(currentDragElement, afterElement);
+    }
+  }
+}
+
+function getDragAfterElement(y) {
+  const draggableElements = Array.from(todoList.querySelectorAll('.todo:not(.dragging)'));
+  return draggableElements.reduce((closest, child, i) => {
+      const box = child.getBoundingClientRect();
+      const boxMidPointHeight = box.top + (box.height / 2)
+      const offset = y - boxMidPointHeight
+      if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+      } else {
+          return closest;
+      }
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+
+function getDragElement(e){
+  currentDragElement = this;
+  currentDragElement.classList.add('dragging')
+  startTime = new Date().getTime();
+}
+function removeDragElement(e){
+  currentDragElement.classList.remove('dragging')
+  currentDragElement = null;
+}
+
+
+function handleFooterClicks(e){
+  const target = e.target;
+  if (target.classList.contains('claer-completed-btn')){
+    const completedTodos = document.querySelectorAll('.todo-list .todo .state.completed')
+    completedTodos.forEach(todo => todo.parentNode.remove());
+  } else if (target.classList.contains('active')){
+    const completedTodos = document.querySelectorAll('.todo-list .todo .state.completed')
+    completedTodos.forEach(todo => todo.parentNode.style.display = "none");
+    const activeTodos = document.querySelectorAll('.todo-list .todo .state:not(.completed)')
+    activeTodos.forEach(todo => todo.parentNode.style.display = "grid");
+    document.querySelector('.todo-footer .todo-filters button.selected').classList.remove('selected')
+    target.classList.add('selected')
+  } else if (target.classList.contains('completed')){
+    const activeTodos = document.querySelectorAll('.todo-list .todo .state:not(.completed)')
+    activeTodos.forEach(todo => todo.parentNode.style.display = "none");
+    const completedTodos = document.querySelectorAll('.todo-list .todo .state.completed')
+    completedTodos.forEach(todo => todo.parentNode.style.display = "grid");
+    document.querySelector('.todo-footer .todo-filters button.selected').classList.remove('selected')
+    target.classList.add('selected')
+  } else if (target.classList.contains('all')){
+    const allTodos = document.querySelectorAll('.todo-list .todo')
+    document.querySelector('.todo-footer .todo-filters button.selected').classList.remove('selected')
+    target.classList.add('selected')
+    allTodos.forEach(todo => todo.style.display = "grid");    
+  }
+}
+
+function handleTodolistClicks(e){  
+  const target = e.target;
+  
+  if (target.classList.contains('remove-icon')){
+    const todo = target.parentNode;
+    const completed = todo.children[0].classList.contains('completed');
+    todo.remove()
+    if (!completed) itemsLeftElement.textContent = --itemsLeftNumber
+  } else if (target.classList.contains('state')){
+    const todo = target.parentNode;
+    const completed = target.classList.contains('completed');
+    if (completed){
+      target.classList.remove('completed');
+      itemsLeftElement.textContent = ++itemsLeftNumber
+    } else {
+      target.classList.add('completed')
+      itemsLeftElement.textContent = --itemsLeftNumber
+    }
+  }
+}
 
 function addTask(e){
-  if ((this.classList.contains('todo-add-btn') && todoInput.value) || (e.keyCode == "13" && todoInput.value)){
-    const todo = createTodo(todoInput.value)
-    todoList.insertAdjacentHTML('beforeend', todo);
-    todoInput.value = null
+  if ((this == todoAddBtn && todoInput.value) || (e.keyCode == "13" && todoInput.value)){
+    const todo = createTodo(false, todoInput.value)
+    todoList.appendChild(todo);
+    todoInput.value = null;
+    itemsLeftElement.textContent = ++itemsLeftNumber
   }
 }
 
 
-function createTodo(task){
-  return `<div class="todo">
-            <label class="state"><input type="checkbox"></label>
-            <p>${task}</p>
-            <img src="./images/icon-cross.svg" class="remove-icon" alt="">
-          </div>`
+function createTodo(state, text){
+  const todo = document.createElement('div');
+  todo.classList.add('todo');
+  todo.draggable = true;
+  todo.innerHTML =`<label class="state ${state? 'completed': ''}"><input type="checkbox" ${state? 'checked="true"': ''}></label>
+                    <p>${text}</p>
+                    <img src="./images/icon-cross.svg" class="remove-icon" alt="">`
+  todo.addEventListener('dragstart', getDragElement)          
+  todo.addEventListener('dragend', removeDragElement)          
+  return todo;
 }
 
 
@@ -36,7 +163,6 @@ function toggleTheme(){
   }
   document.documentElement.classList.toggle('light')
   toggleBtn.classList.toggle('light')
-  console.log(localStorage.getItem('prefers-color-scheme'));
 }
 
 
@@ -46,13 +172,19 @@ function checkUserPreference() {
   if (mediaQuery.matches || preferedMode == 'light') {
     document.documentElement.classList.add('light')
     toggleBtn.classList.add('light')
-    console.log('light');
   } else {
     document.documentElement.classList.remove('light')
     toggleBtn.classList.remove('light')
-    console.log('dark');
   }
 }
 
 
-checkUserPreference()
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  checkUserPreference()
+  todos.forEach(todo =>{
+    todoList.insertAdjacentElement('beforeend', createTodo(todo.completed, todo.text))
+    !todo.completed && itemsLeftNumber++
+  })
+  itemsLeftElement.textContent = itemsLeftNumber;
+})
